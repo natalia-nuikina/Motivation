db79c61e0658b064311ccab41858d4649767d943
Наталья Нуйкина <me@nuikina.ru>
2024-12-14 03:27:27 +0300

    ant table

--- src/components/TheMotivation.vue
+++ src/components/TheMotivation.vue
@@ -8,6 +8,7 @@ const post = defineModel('post');
 const organization = defineModel('organization');
 const fullName = defineModel('fullName');
 const personnelNumber = defineModel('personnelNumber');
+
 const filterOptionInput = (input, option) => {
   return option.value.toLowerCase().indexOf(input.toLowerCase()) >= 0;
 };
@@ -16,10 +17,42 @@ const handleOk = (e) => {
   console.log(e);
   store.changeKpi(id.value, kpi.value);
   open.value = false;
+  console.log(kpi)
 };
 const kpi = ref('');
 const comment = ref('');
 const id = ref('');
+
+const columns = [
+  {
+    title: 'Организация',
+    dataIndex: 'organization',
+  },
+  {
+    title: 'Подразделение',
+    dataIndex: 'division',
+  },
+  {
+    title: 'Физическое лицо',
+    dataIndex: 'fullName',
+  },
+  {
+    title: 'GUID Физлица',
+    dataIndex: 'guid',
+  },
+  {
+    title: 'Должность',
+    dataIndex: 'post',
+  },
+  {
+    title: 'Табельный номер',
+    dataIndex: 'personnelNumber',
+  },
+  {
+    title: 'KPI (%)',
+    dataIndex: 'kpi',
+  },
+]
 </script>
 
 <template>
@@ -96,38 +129,16 @@ const id = ref('');
         </div>
       </div>
       <div class="table">
-        <table>
-          <thead>
-            <tr>
-              <th>Организация</th>
-              <th>Подразделение</th>
-              <th>Физическое лицо</th>
-              <th>GUID Физлица</th>
-              <th>Должность</th>
-              <th>Табельный номер</th>
-              <th>KPI (%)</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr
-              v-for="person in store.filteredStaff"
-              :key="person.id"
-            >
-            <td>{{ person.organization }}</td>
-            <td>{{ person.division }}</td>
-            <td>{{ person.fullName }}</td>
-            <td>{{ person.guid }}</td>
-            <td>{{ person.post }}</td>
-            <td>{{ person.personnelNumber }}</td>
-            <td class="kpi">
-              {{ person.kpi }}
-              <a-button @click="open = true, kpi = person.kpi, id = person.id">
-                <EditOutlined />
-              </a-button>
-            </td>
-            </tr>
-          </tbody>
-        </table>
+        <a-table bordered :data-source="store.filteredStaff" :columns="columns">
+          <template #bodyCell="{ column, text, record }">
+            <template v-if="column.dataIndex === 'kpi'">
+              <div class="editable-cell">
+                  {{ text || ' ' }}
+                  <EditOutlined @click="open = true, kpi = text, id = record.id" class="editable-cell-icon"/>
+              </div>
+            </template>
+          </template>
+        </a-table>
       </div>
       <a-modal v-model:open="open" title="KPI" @ok="handleOk">
         <div>Введите новое значение KPI:</div>
@@ -140,32 +151,17 @@ const id = ref('');
 </template>
 
 <style scoped>
-main {
-  display: flex;
-  flex-direction: column;
-}
-/* .filters {
-  display: flex;
-  width: 100vw;
-  flex-wrap: wrap;
-} */
-table {
-  width: 100vw;
-}
 
-th {
-  text-align: start;
-}
-
-tbody tr:hover {
-  background-color: #CDE4FF;
-}
-
-.kpi {
+.editable-cell {
   display: flex;
   align-items: center;
+  justify-content: space-around;
 }
 .select {
   width: 300px;
 }
+
+.editable-cell-icon:hover {
+    color: #108ee9;
+  }
 </style>
